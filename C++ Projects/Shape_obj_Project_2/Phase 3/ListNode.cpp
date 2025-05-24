3b59f43f0ef0d494492d0496289603e026a4986d
Soshi007 <jainshoshi@gmail.com>
2025-05-23 21:42:42 -0400

    Delete C++ Projects/Shape_obj_Project_2/Phase 3 directory

--- C++ Projects/Shape_obj_Project_2/Phase 3/ListNode.cpp
+++
@@ -1,6 +0,0 @@
-//Part 4: Ongshi Dugar
-#include "ListNode.h"
-#include "Person.h"
-template<typename T>
-ListNode<T>::ListNode(T* data) : data(data), next(nullptr){}
-template class ListNode<Person*>; 
--- C++ Projects/Shape_obj_Project_2/Phase 3/ReadMe.txt
+++
@@ -1,1 +0,0 @@
-Team 2 please upload all project 3 files here, thank you.
